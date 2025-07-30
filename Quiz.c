3faaec69dd8e6ce3ea054784b726bb863c50406c
Tales <talesruvio@gmail.com>
2025-07-30 14:14:55 -0300

    Versão 5

--- Quiz.c
+++ Quiz.c
@@ -3,8 +3,10 @@
 char resposta;
 int acertos=0;
 int erros=0;
+int i=0;
 
 void alternativas(int na, int nb, char op, int nc, int a, int b, int c, int d, char co1, char co2){
+    for (i=0; i<999; i++){
     printf("Questao %d: Qual o resultado de %d%c%d?", na,nb,op,nc);
     printf("\nAlternativa A: %d", a);
     printf("\nAlternativa B: %d", b);
@@ -15,10 +17,15 @@ void alternativas(int na, int nb, char op, int nc, int a, int b, int c, int d, c
      if(resposta==co1 || resposta==co2){
                 printf("\nCorreto!\n\n");
                 acertos=acertos+1;
-            }else{
+                break;
+            }else if(resposta=='a' || resposta=='A' || resposta=='b' || resposta=='B' || resposta =='c' || resposta=='C' || resposta=='d' || resposta=='D'){
                 printf("\nErrado! A alternativa correta era: %c\n\n", co2);
                 erros=erros+1;
+                break;
+            }else{
+                printf("\nPor favor, selecione uma alternativa valida(A-D)\n\n");
             }
+        }    
 }
 
 int main(){
@@ -40,6 +47,11 @@ int main(){
             alternativas(1, 18, '+', 42, 50, 60, 70, 80, 'b', 'B');
             alternativas(2, 99, '+', 99, 100, 199, 198, 200, 'c', 'C');
             alternativas(3, 475, '-', 175, 400, 301, 300, 600, 'c', 'C');
+            alternativas(4, 972, '-', 184, 788, 800, 798, 787, 'a', 'A');
+            alternativas(5, 100, '*', 23, 230, 23000, 230000, 2300, 'd', 'D');
+            alternativas(6, 92, '*', 3, 276, 286, 423, 372, 'a', 'A');
+            alternativas(7, 230, '/', 5, 64, 46, 55, 50, 'b', 'B');
+            alternativas(8, 402, '/', 6, 60, 70, 76, 67, 'd', 'D');
             printf("Parabens, %s! Voce chegou ao final do Quiz!\n", nome);
             printf("Voce acertou %d questoes, e errou %d questoes\n\n", acertos, erros);
             for(aus=0; aus<999; aus++){
@@ -51,7 +63,7 @@ int main(){
                     fprintf(ponteiro, "Acertos: %d\n", acertos);
                     fclose(ponteiro);
                     printf("Arquivo criado com sucesso!\n");
-                    printf("Obrigado por usar a calculadora, ate a proxima!\n\n");
+                    printf("Obrigado por realizar o Quiz, ate a proxima!\n\n");
                     break;
                 }else if(r=='n' || r=='N'){
                     printf("Ok, ate uma proxima vez!");
